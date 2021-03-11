b70638f7b1d5e301869ee2592bc03c690bc54498
Widianto Nur F <xnaltasee@gmail.com>
2021-03-11 17:56:55 +0700

    melf: cleanup codebase

--- melf.c
+++ melf.c
@@ -12,38 +12,43 @@ static int valid_elfheader(Elf64_Ehdr *elf_buf)
 
 static char *parse_elfheader_class(Elf64_Ehdr *elf_buf)
 {
-	switch (elf_buf->e_ident[EI_CLASS]) {
-	case ELFCLASS32:
-		return "32";
-	case ELFCLASS64:
-		return "64";
-	}
+	char *class_data[] = {
+		[ELFCLASS32] = "32",
+		[ELFCLASS64] = "64"
+	};
+
+	if (class_data[elf_buf->e_ident[EI_CLASS]])
+		return class_data[elf_buf->e_ident[EI_CLASS]];
 
 	return "Unknown";
 }
 
 static char *parse_elfheader_data(Elf64_Ehdr *elf_buf)
 {
-	switch (elf_buf->e_ident[EI_DATA]) {
-	case ELFDATA2LSB:
-		return "little-endian";
-	case ELFDATA2MSB:
-		return "big-endian";
-	}
+	char *elf_data[] = {
+		[ELFDATA2LSB] = "little-endian",
+		[ELFDATA2MSB] = "big-endian"
+	};
+
+	if (elf_data[elf_buf->e_ident[EI_DATA]])
+		return elf_data[elf_buf->e_ident[EI_DATA]];
 
 	return "Unknown";
 }
 
 static char *parse_elfheader_version(Elf64_Ehdr *elf_buf)
 {
-	return elf_buf->e_ident[EI_VERSION] == EV_CURRENT ? "Current" : "Unknown";
+	if (elf_buf->e_ident[EI_VERSION] == EV_CURRENT)
+		return "Current";
+
+	return "Unknown";
 }
 
 static char *parse_elfheader_osabi(Elf64_Ehdr *elf_buf)
 {
 	char *osabi_data[] = {
 		[ELFOSABI_SYSV] = "UNIX System V",
-		[ELFOSABI_HPUX] = "HP-UX",
+		[ELFOSABI_HPUX] = "Hewlett-Packard HP-UX",
 		[ELFOSABI_NETBSD] = "NetBSD",
 		[ELFOSABI_LINUX] = "Linux",
 		[ELFOSABI_SOLARIS] = "Sun Solaris",
@@ -86,6 +91,17 @@ static char *parse_elfheader_type(Elf64_Ehdr *elf_buf)
 
 /* TODO: e_machine (architecture) function */
 
+#define parse_elfheader_entry(e)  e->e_entry
+#define parse_elfheader_phoff(e)  e->e_phoff
+#define parse_elfheader_shoff(e)  e->e_shoff
+#define parse_elfheader_flags(e)  e->e_flags
+#define parse_elfheader_ehsize(e)  e->e_ehsize
+#define parse_elfheader_phentsize(e)  e->e_phentsize
+#define parse_elfheader_phnum(e)  e->e_phnum
+#define parse_elfheader_shentsize(e)  e->e_shentsize
+#define parse_elfheader_shnum(e)  e->e_shnum
+#define parse_elfheader_shstrndx(e)  e->e_shstrndx
+
 static void dump_elfheader(Elf64_Ehdr *elf_buf)
 {
 	printf("Magic   : ");
@@ -94,22 +110,38 @@ static void dump_elfheader(Elf64_Ehdr *elf_buf)
 		printf("%02x ", elf_buf->e_ident[i]);
 	printf("\n");
 
-	printf("Class                              : ELF%s\n", parse_elfheader_class(elf_buf));
-	printf("Data                               : %s\n", parse_elfheader_data(elf_buf));
-	printf("Version                            : %s\n", parse_elfheader_version(elf_buf));
-	printf("OS/ABI                             : %s\n", parse_elfheader_osabi(elf_buf));
-	printf("ABI version                        : %#x\n", parse_elfheader_abiver(elf_buf));
-	printf("Type                               : %s\n", parse_elfheader_type(elf_buf));
-	printf("Entry virtual address              : %#lx\n", elf_buf->e_entry);
-	printf("Program header table's file offset : %lu bytes\n", elf_buf->e_phoff);
-	printf("Section header table's file offset : %lu bytes\n", elf_buf->e_shoff);
-	printf("Flags                              : %#x\n", elf_buf->e_flags);
-	printf("Size of this ELF header            : %u bytes\n", elf_buf->e_ehsize);
-	printf("Size of program header table       : %u bytes\n", elf_buf->e_phentsize);
-	printf("Number of program header table     : %u\n", elf_buf->e_phnum);
-	printf("Size of section header's           : %u bytes\n", elf_buf->e_shentsize);
-	printf("Number of section header table     : %u\n", elf_buf->e_shnum);
-	printf("Section header table index         : %u\n", elf_buf->e_shstrndx);
+	printf("Class                              : ELF%s\n",
+				parse_elfheader_class(elf_buf));
+	printf("Data                               : %s\n",
+				parse_elfheader_data(elf_buf));
+	printf("Version                            : %s\n",
+				parse_elfheader_version(elf_buf));
+	printf("OS/ABI                             : %s\n",
+				parse_elfheader_osabi(elf_buf));
+	printf("ABI version                        : %#x\n",
+				parse_elfheader_abiver(elf_buf));
+	printf("Type                               : %s\n",
+				parse_elfheader_type(elf_buf));
+	printf("Entry virtual address              : %#lx\n",
+				parse_elfheader_entry(elf_buf));
+	printf("Program header table's file offset : %lu bytes\n",
+				parse_elfheader_phoff(elf_buf));
+	printf("Section header table's file offset : %lu bytes\n",
+				parse_elfheader_shoff(elf_buf));
+	printf("Flags                              : %#x\n",
+				parse_elfheader_flags(elf_buf));
+	printf("Size of this ELF header            : %u bytes\n",
+				parse_elfheader_ehsize(elf_buf));
+	printf("Size of program header table       : %u bytes\n",
+				parse_elfheader_phentsize(elf_buf));
+	printf("Number of program header table     : %u\n",
+				parse_elfheader_phnum(elf_buf));
+	printf("Size of section header's           : %u bytes\n",
+				parse_elfheader_shentsize(elf_buf));
+	printf("Number of section header table     : %u\n",
+				parse_elfheader_shnum(elf_buf));
+	printf("Section header table index         : %u\n",
+				parse_elfheader_shstrndx(elf_buf));
 }
 
 int main(int argc, char **argv)
